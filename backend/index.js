ca514569e5c455ca4f7dcf6da69df14d5ac53183
Michael-Jean-Wonde <michaeljeanwonde1839@gmail.com>
2022-07-21 23:02:16 +0300

    Routes and Controllers commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import gigRoutes from './routes/gigRoutes.js'
-import jobRoutes from './routes/jobRoutes.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import gigRoutes from './routes/gigRoutes.js';
+import jobRoutes from './routes/jobRoutes.js';
+import getjobRoutes from './routes/getjobRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -16,6 +18,29 @@ app.get('/', (req,res)=>{
 
 app.use('/gig', gigRoutes);
 app.use('/postjob', jobRoutes);
+app.use('/getjob',getjobRoutes);
+app.use('/user',userRoutes);
+
+
+app.use((req,res,next)=>{
+    const error = new Error('not found')
+    res.status(404)
+    next(error)
+})
+
+app.use((req,res,next)=>{
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message
+    })
+})
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
